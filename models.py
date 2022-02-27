1e1518e71d4d5eee1a328889c0fd20730b37d609
Surya Prakash Mishra <mishrasp393@gmail.com>
2022-02-28 01:08:57 +0530

    fix: head

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -24,31 +23,4 @@ class UserModel(UserMixin, db.Model):
  
 @login.user_loader
 def load_user(id):
-=======
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager
- 
-login = LoginManager()
-db = SQLAlchemy()
- 
-class UserModel(UserMixin, db.Model):
-    _tablename_ = 'users'
- 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(80), unique=True)
-    username = db.Column(db.String(100))
-    password_hash = db.Column(db.String())
- 
-    def set_password(self,password):
-        self.password_hash = generate_password_hash(password)
-     
-    def check_password(self,password):
-        return check_password_hash(self.password_hash,password)
- 
- 
-@login.user_loader
-def load_user(id):
->>>>>>> 5f03060fac5de771be6c18ff3c1d1bebf250bafb
     return UserModel.query.get(int(id))
